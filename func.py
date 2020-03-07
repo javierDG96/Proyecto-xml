7578af577e20e39526eef12f8246e15e1e7baeb1
Javier Domingo Gonzalez <Javi_dg@hotmail.es>
2020-03-07 20:07:13 +0100

    He añadido la 2 funcion

--- func.py
+++ func.py
@@ -5,3 +5,6 @@ def listar_nomtil(prueba):
 	titulo=prueba.xpath("//Champion/title/text()")
 	return zip(nombre,titulo)
 
+def contar_camp(prueba):
+	cant=prueba.xpath("//Champion/name/text()")
+	return len(cant)
